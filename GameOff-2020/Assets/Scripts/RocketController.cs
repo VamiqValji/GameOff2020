bfd73a68bd1d5c35b6be9e34bf2d5c8763f8133b
Vamiq <vamiq.valji@gmail.com>
2020-11-08 18:36:25 -0500

    Rocket movement
    After LOTS of playing around with the rocket's movement, this will do for now. I will come back and change it if I believe it needs any re-working, which it probably will.

--- GameOff-2020/Assets/Scripts/RocketController.cs
+++ GameOff-2020/Assets/Scripts/RocketController.cs
@@ -8,6 +8,11 @@ public class RocketController : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
     private Vector3 respawnPoint;
+    public float rocketRotation = 0.8f;
+    public float rocketForce = 20f;
+    public float movementX = 0f;
+    public float maxYVelocity = 5f;
+    public float maxXVelocity = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +23,43 @@ public class RocketController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Horizontal") == true)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, speed);
-        }
+        Debug.Log("x: " + rb.velocity.x);
+        Debug.Log("y: " + rb.velocity.y);
     }
 
     public void FixedUpdate()
     {
-        
+        movementX = -(Input.GetAxis("Horizontal"));
+        if ((Input.GetButton("Horizontal") == true) & (movementX != 0))
+        {
+            //rb.velocity = new Vector2(rb.velocity.x * movementX, speed);
+            rb.AddForce(transform.up * rocketForce);
+            //ConstantForce2D = (rocketForce) 
+            //rb.rotation = rocketRotation;
+            //transform.Rotate(transform.rotation.x, transform.rotation.y, transform.rotation.z * rocketRotation);
+            transform.Rotate(0.0f, 0.0f, rocketRotation * movementX * Time.deltaTime, Space.Self);
+            // MAX OUT VELOCITY
+            if (rb.velocity.y > maxYVelocity)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, maxYVelocity);
+            }
+            //if (rb.velocity.y < -maxYVelocity)
+            //{
+            //    rb.velocity = new Vector2(rb.velocity.x, -maxYVelocity);
+            //}
+            if (rb.velocity.x > maxXVelocity)
+            {
+                rb.velocity = new Vector2(maxXVelocity, rb.velocity.y);
+            }
+            if (rb.velocity.x < -maxXVelocity)
+            {
+                rb.velocity = new Vector2(-maxXVelocity, rb.velocity.y);
+            }
+            if (rb.velocity.x != 0f)
+            {
+                //rb.velocity = new Vector2(-movementX * 5, rb.velocity.y);
+                rb.AddForce(transform.right * movementX * 1.5f);
+            }
+        }
     }
 }
