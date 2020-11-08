03847fea21576ca1d4e9ea05e7450e1fc5944cf3
Vamiq <vamiq.valji@gmail.com>
2020-11-08 17:27:25 -0500

    Billy the kid & very basic movement
    I imported Billy the kid to actually be in the game. I got the VERY basic movement of the rocket done (without the rotations).

---
+++ GameOff-2020/Assets/Scripts/RocketController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private Vector3 respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        respawnPoint = rb.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        
+    }
+}
