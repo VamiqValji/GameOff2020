5d01986da80f61ab63e6fea38a4b0e836f2b2c57
Vamiq <vamiq.valji@gmail.com>
2020-11-14 22:04:48 -0500

    Cloud Boss Update
    The cloud boss currently spawns correctly, though it also needs to be able to follow the player on the X axis and Y axis.

---
+++ GameOff-2020/Assets/Scripts/CloudBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBoss : MonoBehaviour
+{
+    Transform player;
+    public float Distance = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        //transform.position = new Vector2(player.position.x, player.position.y + Distance);
+    }
+}
