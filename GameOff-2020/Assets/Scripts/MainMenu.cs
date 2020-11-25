49e9633150d09af8eb9ccc3d93e28231d65a8f8f
Vamiq <vamiq.valji@gmail.com>
2020-11-24 20:05:23 -0500

    Set up basic menu
    I got the basic menu screen set up. It is likely that this will be redesigned.

---
+++ GameOff-2020/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    // MAIN MENU
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Will quit but not in Unity editor");
+        Application.Quit();
+    }
+
+    // OPTIONS
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
