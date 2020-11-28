85dc01343889b040ef21b08f5ed29c69f03186e3
Vamiq <vamiq.valji@gmail.com>
2020-11-28 15:11:45 -0500

    Boss Indicator
    I definitely spent too much time on this feature, but a sound will play  and there will be a visual indicator on the screen, that a boss will appear very shortly when the player is near a boss fight.

--- GameOff-2020/Assets/Scripts/MiddleBottomSideCheck.cs
+++ GameOff-2020/Assets/Scripts/MiddleBottomSideCheck.cs
@@ -13,6 +13,9 @@ public class MiddleBottomSideCheck : MonoBehaviour
     public Transform particleObject;
     //public ParticleSystem Particles;
     public float particlesAboveSpeed = 20f;
+    public Animator BIncomingAnimator;
+    private bool entry = true;
+    public GameObject BossIncomingSound;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +35,9 @@ public class MiddleBottomSideCheck : MonoBehaviour
             score = 0;
             highscore = 0;
             bottomSideCheck.position = respawnPoint;
+            entry = true;
+            BossIncomingSound.SetActive(false);
+            BIncomingAnimator.SetTrigger("HardReset");
         }
         if (score > highscore)
         {
@@ -39,5 +45,48 @@ public class MiddleBottomSideCheck : MonoBehaviour
             //Debug.Log(new Vector3(bottomSideCheck.position.x, bottomSideCheck.position.y - distance, bottomSideCheck.position.z));
             bottomSideCheck.localPosition = new Vector3(8, player.position.y - distance, 0);
         }
+
+        if (score > 87 && score < 90 && entry == true) // Cloud Boss
+        {
+            SetAnim();
+        }
+        if (score > 87 + 30 && entry == false)
+        {
+            ResetAnim();
+        }
+
+        if (score > 267 && score < 270 && entry == true) // Demon Boss
+        {
+            SetAnim();
+        }
+        if (score > 267 + 30 && entry == false)
+        {
+            ResetAnim();
+        }
+
+        if (score > 417 && score < 420 && entry == true) // Unicorn Boss
+        {
+            SetAnim();
+        }
+        if (score > 417 + 30 && entry == false)
+        {
+            ResetAnim();
+        }
+
+    }
+
+    private void SetAnim()
+    {
+        entry = false;
+        BIncomingAnimator.SetTrigger("FadeIn");
+        //Instantiate(BossIncomingSound,transform.position,transform.rotation);
+        BossIncomingSound.SetActive(true);
+    }
+
+    private void ResetAnim()
+    {
+        entry = true;
+        BIncomingAnimator.ResetTrigger("FadeIn");
+        BossIncomingSound.SetActive(false);
     }
 }
